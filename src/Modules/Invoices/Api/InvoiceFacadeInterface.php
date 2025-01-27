5364f22c96489d10956ade2b63b011505409b625
Przemyslaw Perzyna <avan.radien@gmail.com>
2025-01-27 13:39:01 +0100

    Add type to $lines param of InvoiceFacadeInterface->create

--- src/Modules/Invoices/Api/InvoiceFacadeInterface.php
+++ src/Modules/Invoices/Api/InvoiceFacadeInterface.php
@@ -3,11 +3,13 @@
 namespace Modules\Invoices\Api;
 
 use Modules\Invoices\Api\Dtos\InvoiceData;
+use Modules\Invoices\Api\Dtos\InvoiceLineCreateData;
 
 interface InvoiceFacadeInterface
 {
     public function get(string $id): ?InvoiceData;
 
+    /** @param InvoiceLineCreateData[] $lines */
     public function create(string $customerName, string $customerEmail, array $lines): string;
 
     public function send(string $id, ?string $subject = null, ?string $email = null): void;
